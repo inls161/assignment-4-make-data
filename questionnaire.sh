1af5f3ac5649dc52fd2b93efb1260613d5e12a7f
jmullan <jmullan@live.unc.edu>
2016-07-12 13:01:38 -0400

    questionnaire

---
+++ questionnaire.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#This is  a questionnaire that allows for user input of answers. Then we will turn the answers into a .csv file. 
+echo "What is your Github Username?"
+read ans >> answers.list
+
+echo "Do you have a car?"
+read ans1
+echo $ans1 >> answers.list 
+
+echo "Do you live on campus?"
+read ans2
+echo $ans2 >> answers.list
+
+echo "Do you have a college scholarship?"
+read ans3
+echo $ans3 >> answers.list
+
+echo "How many pets do you have?"
+read ans4
+echo $ans4 >> answers.list
+
+echo "How many siblings do you have?"
+read ans5
+echo $ans5 >> answers.list 
+
+paste -d, -s answers.list >> answers.csv
+ 
+
+
+
