2ef567dd918df6bd47b2b0e6b78b1d12d7cbf22e
clasch <clasch@live.unc.edu>
2016-07-13 12:01:51 -0400

    completed script

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 #This is  a questionnaire that allows for user input of answers. Then we will turn the answers into a .csv file. 
+TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+
 echo "What is your Github Username?"
-read ans > answers.list
+read ans
+echo $ans > answers.list
 
 echo "Do you have a car?"
 read ans1
@@ -22,7 +25,9 @@ echo $ans4 >> answers.list
 
 echo "How many siblings do you have?"
 read ans5
-echo $ans5 >> answers.list 
+echo $ans5 >> answers.list
+
+echo $TIMESTAMP >> answers.list
 
 paste -d, -s answers.list >> answers.csv
  
